3143f49f0f19a40d87bb36be4deff50371e9d4e3
johncheairs <48799355+johncheairs@users.noreply.github.com>
2020-04-17 09:27:01 -0500

    Through 12

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 
 @NgModule({
@@ -28,12 +29,15 @@ import { WelcomeComponent } from './home/welcome.component';
     RouterModule.forRoot([
       //Router uses first-match wins strategy
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ], {useHash: true})
   ],
+  //Only used in the app module or root module
   bootstrap: [AppComponent]
 })
 export class AppModule { }
